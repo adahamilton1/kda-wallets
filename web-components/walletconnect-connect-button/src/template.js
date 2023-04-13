3d0fa1ad0e4b72c92c47ec3e215635b6bb03e242
adahamilton1 <jd52691314@gmail.com>
2023-04-13 17:58:28 +0800

    more doc

--- web-components/walletconnect-connect-button/src/template.js
+++ web-components/walletconnect-connect-button/src/template.js
@@ -1,6 +1,3 @@
-// need additional section container within dialog because
-// you cant set display: property of dialog or it'll show
-
 export const TEMPLATE = `
   <button type="button">
     WalletConnect
